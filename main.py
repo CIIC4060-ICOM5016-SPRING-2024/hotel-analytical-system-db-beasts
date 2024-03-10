e1c2b9171560ee9d5ba13e709bb8e181c920c285
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-03-09 23:58:46 -0400

    testing heroku connection

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 
 CORS(app)
 
+
 @app.route('/')
 def greeting():
     return 'Hello, this is the hotel-analytical-system-db-beasts app'
+
 
 if __name__ == '__main__':
     app.run()
