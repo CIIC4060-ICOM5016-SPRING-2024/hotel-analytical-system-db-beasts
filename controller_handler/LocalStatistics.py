b543a0049c0049e8987d688c91e0d86b504991b8
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-03-30 01:53:08 -0400

    Add LocalStatistics_Controller_Handler in controller_handler and # * ROOMTYPE = def Get_post_RoomType(self, hid, employee_id)
    The new controller handler, LocalStatistics_Controller_Handler is added in the controller_handler module. This handler includes methods for handling HTTP requests related to the local statistics of a hotel, including checking user permissions and returning the requested data in a suitable format.

---
+++ controller_handler/LocalStatistics.py
@@ -0,0 +1,63 @@
+from model_dao.LocalStatistics import LocalStatistics_Model_Dao
+from model_dao.employee import Employee_Model_Dao
+from model_dao.hotel import Hotel_Model_Dao
+
+from flask import jsonify
+
+
+class LocalStatistics_Controller_Handler:
+
+    # * ROOMTYPE
+    def RoomType_Dict(self, r):
+        roomtype_dict = {
+            'hotel': r[0],
+            'room type': r[1],
+            'total reserves': r[2]
+        }
+        return roomtype_dict
+
+    # * ROOMTYPE
+    def Get_post_RoomType(self, hid, employee_id):
+        # ** Check if there is a credential
+        if len(employee_id) != 1:
+            return jsonify(Error="Invalid Data"), 400
+        # ** Check if the searched hotel exists
+        daoH = Hotel_Model_Dao()
+        hotel = daoH.Get_Hotel(hid)
+        if not hotel:
+            return jsonify(Error="Hotel not found"), 404
+        # ** Check if the employee exists and their information
+        eid = employee_id['eid']
+        daoE = Employee_Model_Dao()
+        employee = daoE.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found"), 404
+        daoLS = LocalStatistics_Model_Dao()
+        # ** Check employee position
+        if employee[5] == "Regular":
+            # ** Check if the employee works at the hotel that are looking for
+            if employee[1] != hid:
+                return jsonify(Error="Employee is not part of the hotel."), 404
+            roomtypes = daoLS.Get_post_RoomType(hid)
+            result = []
+            for roomtype in roomtypes:
+                result.append(self.RoomType_Dict(roomtype))
+            return jsonify(Total_Hotel_Reserves=result), 200
+        elif employee[5] == "Supervisor":
+            # ** Check the hotel information where the employee works
+            daoH1 = Hotel_Model_Dao()
+            hotel1 = daoH1.Get_Hotel(employee[1])
+            # ** Check if the employee works for the same chain as the searched hotel.
+            if hotel1[1] != hotel[1]:
+                return jsonify(Error=f"Hotel not part of a chain. {hotel1[1]} != {hotel[1]}"), 404
+            roomtypes = daoLS.Get_post_RoomType(hid)
+            result = []
+            for roomtype in roomtypes:
+                result.append(self.RoomType_Dict(roomtype))
+            return jsonify(Total_Hotel_Reserves=result), 200
+        # ** If the employee is an administrator
+        roomtypes = daoLS.Get_post_RoomType(hid)
+        result = []
+        for roomtype in roomtypes:
+            result.append(self.RoomType_Dict(roomtype))
+        return jsonify(Total_Hotel_Reserves=result), 200
