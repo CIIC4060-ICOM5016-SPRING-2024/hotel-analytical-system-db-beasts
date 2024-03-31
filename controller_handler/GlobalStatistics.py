1f10724d832ea1487c0157fbcc601e9cdd52ad4e
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-03-31 01:32:59 -0400

    Add GlobalStatistics.py with PaymentMethod handling
    Added a new file, GlobalStatistics.py, containing GlobalStatistics_Model_Dao. It includes a 'Get_post_PaymentMethod' method which retrieves payment method data and related statistics from the database. Assured checks for valid data and administrator credentials are also implemented.

---
+++ controller_handler/GlobalStatistics.py
@@ -0,0 +1,41 @@
+from model_dao.GlobalStatistics import GlobalStatistics_Model_Dao
+from model_dao.employee import Employee_Model_Dao
+
+from flask import jsonify
+
+
+class GlobalStatistics_Controller_Handler:
+
+    # * PAYMENTMETHOD
+    def PaymentMethod_Dict(self, r):
+        paymentmethod_dict = {
+            'paymentmethod': r[0],
+            'total reservations': r[1],
+            'reservation percentage': r[2]
+        }
+        return paymentmethod_dict
+
+    # * PAYMENTMETHOD
+
+    def Get_post_PaymentMethod(self, employee_id):
+        # ** Check if there is a credential
+        if len(employee_id) != 1:
+            return jsonify(Error="Invalid Data"), 400
+        # if type(employee_id) is not int:
+        #     return jsonify(Error="Invalid Data"), 400
+        # ** Check if the employee exists and their information
+        eid = employee_id['eid']
+        daoE = Employee_Model_Dao()
+        employee = daoE.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found"), 404
+        # ** Check employee position
+        if employee[5] != "Administrator":
+            return jsonify(Error=f"You are not an Administrator. {employee[5]}"), 403
+        # ** All Good
+        daoGS = GlobalStatistics_Model_Dao()
+        paymentmethods = daoGS.Get_post_PaymentMethod()
+        result = []
+        for paymentmethod in paymentmethods:
+            result.append(self.PaymentMethod_Dict(paymentmethod))
+        return jsonify(Payment_Method_Percentage=result), 200
