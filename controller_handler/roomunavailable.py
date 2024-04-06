939fff03f128479259c4b4a421d52440ca5f3dd1
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-06 19:36:21 -0400

    Add method to post room unavailability
    A new method 'Post_RoomUnavailable' was added to the RoomUnavailable_Controller_Handler. This method checks employee information, room availability, and validates the unavailability time span. It creates a new room unavailability entry for the indicated dates if all checks pass successfully. The code was refactored to include additional necessary modules.

--- controller_handler/roomunavailable.py
+++ controller_handler/roomunavailable.py
@@ -1,7 +1,10 @@
 # ** Importing necessary modules
 from model_dao.roomunavailable import RoomUnavailable_Model_Dao
-from flask import jsonify
+from model_dao.room import Room_Model_Dao
+from model_dao.employee import Employee_Model_Dao
 
+from flask import jsonify
+from datetime import datetime
 
 # ** Class for handling HTTP requests related to RoomUnavailable
 class RoomUnavailable_Controller_Handler:
@@ -14,6 +17,15 @@ class RoomUnavailable_Controller_Handler:
             'enddate': r[3]
         }
         return roomunavailable_dict
+
+    def RoomUnavailable_Build(self, ruid, rid, startdate, enddate):
+        roomunavailable_build = {
+            'ruid': ruid,
+            'rid': rid,
+            'startdate': startdate,
+            'enddate': enddate
+        }
+        return roomunavailable_build
 
     """
     ------------------
@@ -38,3 +50,46 @@ class RoomUnavailable_Controller_Handler:
             result = self.RoomUnavailable_Dict(roomunavailable)
             return jsonify(Roomunavailable=result)
         return jsonify(Error="Not Found"), 404
+
+    def Post_RoomUnavailable(self, roomunavailable_data):
+        if len(roomunavailable_data) != 4:
+            return jsonify(Error="Invalid Data"), 400
+
+        # ** Checking employee information
+        eid = roomunavailable_data['eid']
+        daoEmployee = Employee_Model_Dao()
+        employee = daoEmployee.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found."), 404
+        if employee[5] != "Supervisor":
+            return jsonify(Error="The employee position is not Supervisor"), 401
+
+        # ** Checking room based on the hotel where the employee works
+        rid = roomunavailable_data['rid']
+        daoRoom = Room_Model_Dao()
+        room = daoRoom.Get_Room_Info(rid, employee[1])
+        if not room:
+            return jsonify(Error=f"Room not found in the hotel = {employee[1]}."), 404
+
+        # ** Checking length of unavailable
+        startdate = roomunavailable_data['startdate']
+        enddate = roomunavailable_data['enddate']
+        if enddate <= startdate:
+            return jsonify(Error="End date must be greater than start date"), 400
+
+        # ** Checking unavailable
+        daoRU1 = RoomUnavailable_Model_Dao()
+        not_disponibility = daoRU1.RoomUnavailable_Time(rid)
+        startdate_date = datetime.strptime(startdate, '%Y-%m-%d').date()
+        if not_disponibility[0] >= startdate_date:
+            return jsonify(Error=f"Room is not available during the selected dates. "
+                                 f"not_disponibility={not_disponibility[0]} vs "
+                                 f"startdate={startdate}"), 400
+
+        # ** Creating new room unavailability for the indicated dates
+        daoRU2 = RoomUnavailable_Model_Dao()
+        ruid = daoRU2.Post_RoomUnavailable(rid, startdate, enddate)
+        if not ruid:
+            return jsonify(Error="The room could not be separated."), 404
+        roomunavailable_result = self.RoomUnavailable_Build(ruid, rid, startdate, enddate)
+        return jsonify(Roomunavailable=roomunavailable_result), 201
