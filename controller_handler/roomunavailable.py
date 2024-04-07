0ebbd06405a9bb7992fc96c0ea688ab1034ade01
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-06 20:53:52 -0400

    Put_RoomUnavailable = half done

--- controller_handler/roomunavailable.py
+++ controller_handler/roomunavailable.py
@@ -93,3 +93,48 @@ class RoomUnavailable_Controller_Handler:
             return jsonify(Error="The room could not be separated."), 404
         roomunavailable_result = self.RoomUnavailable_Build(ruid, rid, startdate, enddate)
         return jsonify(Roomunavailable=roomunavailable_result), 201
+
+    def Put_RoomUnavailable(self, ruid, roomunavailable_data):
+        if len(roomunavailable_data) != 4:
+            return jsonify(Error="Invalid Data"), 400
+
+        # ** Checking employee information
+        eid = roomunavailable_data['eid']
+        daoEmployee = Employee_Model_Dao()
+        employee = daoEmployee.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found."), 404
+        if employee[5] != "Supervisor":
+            return jsonify(Error="The employee position is not Supervisor"), 401
+
+        daoRU = RoomUnavailable_Model_Dao()
+        ruid1 = daoRU.Get_RoomUnavailable(ruid)
+        if not ruid1:
+            return jsonify(Error="RoomUnavailable not found."), 404
+
+        # ** Checking room based on the hotel where the employee works
+        rid = roomunavailable_data['rid']
+        daoRoom = Room_Model_Dao()
+        room = daoRoom.Get_Room_Info(rid, employee[1])
+        if not room:
+            return jsonify(Error=f"Room not found in the hotel = {employee[1]}."), 404
+
+        # ** Checking length of unavailable
+        startdate = roomunavailable_data['startdate']
+        enddate = roomunavailable_data['enddate']
+        if enddate <= startdate:
+            return jsonify(Error="End date must be greater than start date"), 400
+
+        # ** Checking unavailable
+        daoRU1 = RoomUnavailable_Model_Dao()
+        not_disponibility = daoRU1.RoomUnavailable_Time(rid)
+        startdate_date = datetime.strptime(startdate, '%Y-%m-%d').date()
+        if not_disponibility[0] >= startdate_date:
+            return jsonify(Error=f"Room is not available during the selected dates. "
+                                 f"not_disponibility={not_disponibility[0]} vs "
+                                 f"startdate={startdate}"), 400
+
+        daoRU2 = RoomUnavailable_Model_Dao()
+        roomunavailable = daoRU2.Put_RoomUnavailable(ruid, rid, startdate, enddate)
+        result = self.RoomUnavailable_Build(ruid, rid, startdate, enddate)
+        return jsonify(RoomUnavailable=result)
