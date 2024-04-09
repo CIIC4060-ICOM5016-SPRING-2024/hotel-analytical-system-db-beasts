e204c30c97b5480665c503041293eb8d12435db2
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-09 01:29:44 -0400

    Refactor hotel controller methods
    Rearranged the logic and cleaned up the code in the 'Put_Hotel' and 'Delete_Hotel' methods in the Hotel controller. These changes include optimized error handling and the elimination of unnecessary condition checks. All changes aim to improve readability and maintainability of the code.

--- controller_handler/hotel.py
+++ controller_handler/hotel.py
@@ -76,36 +76,37 @@ class Hotel_Controller_Handler:
     def Put_Hotel(self, hid, hotel_data):
         if len(hotel_data) != 3:
             return jsonify(Error="Invalid Data"), 400
-        # ** Models/Daos to use
+
+        daoH = Hotel_Model_Dao()
+        if not daoH.Get_Hotel(hid):
+            return jsonify(Error="Hotel not found"), 404
+
         daoc = Chains_Model_Dao()
-        daoh = Hotel_Model_Dao()
-        # ** Data received
         chid = hotel_data['chid']
-        hname = hotel_data['hname']
-        hcity = hotel_data['hcity']
-        # ** Search chid if exists
         if not daoc.Get_Chain(chid):
             return jsonify(Error="Chain not found"), 404
-        if (hid or hid == 0) and hname and hcity:
-            hotel = daoh.Put_Hotel(hid, chid, hname, hcity)
+
+        hname = hotel_data['hname']
+        hcity = hotel_data['hcity']
+        if hname and hcity:
+            daoH1 = Hotel_Model_Dao()
+            hotel = daoH1.Put_Hotel(hid, chid, hname, hcity)
             result = self.Hotel_Build(hid, chid, hname, hcity)
-            if hotel:
-                return jsonify(Hotel=result), 200
-            else:
-                return jsonify(Error="Hotel not found"), 404
+            return jsonify(Hotel=result), 200
         else:
             return jsonify(Error="Unexpected attribute values."), 400
 
     # ** Method to delete an existing hotel
     def Delete_Hotel(self, hid):
-        if hid or hid == 0:
-            dao = Hotel_Model_Dao()
-            result = dao.Delete_Hotel(hid)
-            if result == "Error deleting":
-                return jsonify(Error="Hotel is referenced"), 400
-            elif result:
-                return jsonify(OK="Deleted"), 200
-            else:
-                return jsonify(Error="Not Found"), 404
+        daoH = Hotel_Model_Dao()
+        if not daoH.Get_Hotel(hid):
+            return jsonify(Error="Hotel not found"), 404
+
+        daoH1 = Hotel_Model_Dao()
+        result = daoH1.Delete_Hotel(hid)
+        if result == "Error deleting":
+            return jsonify(Error="Hotel is referenced"), 400
+        elif result:
+            return jsonify(OK="Deleted"), 200
         else:
-            return jsonify(Error="Error deleting"), 400
+            return jsonify(Error="Delete Failed"), 500
