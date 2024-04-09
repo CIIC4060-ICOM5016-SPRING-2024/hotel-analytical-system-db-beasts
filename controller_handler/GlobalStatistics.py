3a99182e4cced5432e9373088d5cbaf27e296e02
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-09 18:14:17 -0400

    Add MostRevenue feature to GlobalStatistics
    A new feature named MostRevenue has been added to GlobalStatistics.py. It includes a new dictionary function, MostRevenue_Dict, and a new GET POST method, Get_post_MostRevenue. This feature provides and verifies the total revenue for each employee_id, specifically for Administrator position.

--- controller_handler/GlobalStatistics.py
+++ controller_handler/GlobalStatistics.py
@@ -15,8 +15,15 @@ class GlobalStatistics_Controller_Handler:
         }
         return paymentmethod_dict
 
-    # * PAYMENTMETHOD
+    # * MOST_REVENUE
+    def MostRevenue_Dict(self, r):
+        mostrevenue_dict = {
+            'chid': r[0],
+            'total revenue': r[1]
+        }
+        return mostrevenue_dict
 
+    # * PAYMENTMETHOD
     def Get_post_PaymentMethod(self, employee_id):
         # ** Check if there is a credential
         if len(employee_id) != 1:
@@ -39,3 +46,26 @@ class GlobalStatistics_Controller_Handler:
         for paymentmethod in paymentmethods:
             result.append(self.PaymentMethod_Dict(paymentmethod))
         return jsonify(Payment_Method_Percentage=result), 200
+
+    # * MOST_REVENUE
+    def Get_post_MostRevenue(self, employee_id):
+        # ** Check if there is a credential
+        if len(employee_id) != 1:
+            return jsonify(Error="Invalid Data"), 400
+        # if type(employee_id) is not int:
+        #     return jsonify(Error="Invalid Data"), 400
+        # ** Check if the employee exists and their information
+        eid = employee_id['eid']
+        daoE = Employee_Model_Dao()
+        employee = daoE.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found"), 404
+        # ** Check employee position
+        if employee[5] != "Administrator":
+            return jsonify(Error=f"You are not an Administrator. {employee[5]}"), 403
+        daoGS = GlobalStatistics_Model_Dao()
+        mostrevenues = daoGS.Get_post_MostRevenue()
+        result = []
+        for mostrevenue in mostrevenues:
+            result.append(self.MostRevenue_Dict(mostrevenue))
+        return jsonify(Most_Revenues=result), 200
