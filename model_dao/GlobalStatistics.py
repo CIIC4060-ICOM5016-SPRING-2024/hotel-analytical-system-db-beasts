fb77f6595cbb730e7167e249adb3598d2dfbf91b
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-10 17:06:16 -0400

    Add MostProfitMonth method to GlobalStatistics model
    A new method named "Get_post_MostProfitMonth" has been added to the GlobalStatistics model. This method will extract and return the details of the month with the most hotel reservations across all chains. The top 3 results are fetched and sorted in descending order based on total hotel room reservations.

--- model_dao/GlobalStatistics.py
+++ model_dao/GlobalStatistics.py
@@ -81,7 +81,28 @@ class GlobalStatistics_Model_Dao:
                  "GROUP BY hid, hname "
                  "ORDER BY total_reserves desc "
                  "limit (SELECT ceil(0.1 * (SELECT count(hid)-1 "
-                 "FROM hotel)))")
+                 "       FROM hotel)))")
+        cur.execute(query)
+        result = cur.fetchall()
+        self.db.close()
+        cur.close()
+        return result
+
+    # * MOST_PROFITMONTH
+    def Get_post_MostProfitMonth(self):
+        cur = self.db.docker_connection.cursor()
+        query = ("SELECT chid, "
+                 "       cname, "
+                 "       (SELECT EXTRACT(MONTH FROM startdate)) AS month, "
+                 "       count(reid) as total_reserves "
+                 "FROM reserve "
+                 "NATURAL INNER JOIN roomunavailable "
+                 "NATURAL INNER JOIN room "
+                 "NATURAL INNER JOIN hotel "
+                 "NATURAL INNER JOIN chains "
+                 "GROUP BY chid, cname, month "
+                 "ORDER BY total_reserves desc "
+                 "LIMIT 3 ")
         cur.execute(query)
         result = cur.fetchall()
         self.db.close()
