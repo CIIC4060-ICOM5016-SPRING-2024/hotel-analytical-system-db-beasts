729904e7ad700ea309de5376b308641b2b49518a
Samir Y. Ali-Rivera <70657716+samiralirivera@users.noreply.github.com>
2024-04-09 21:08:03 -0400

    added, global and local statistics for model_dao and controller_handler

--- controller_handler/LocalStatistics.py
+++ controller_handler/LocalStatistics.py
@@ -16,6 +16,18 @@ class LocalStatistics_Controller_Handler:
         }
         return roomtype_dict
 
+    # * MOSTCREDITCARD
+    def MostCreditCard_Dict(self, r):
+        mostcreditcard_dict = {
+            'clid': r[0],
+            'age': r[1],
+            'hid': r[2],
+            'chid': r[3],
+            'payment': r[4],
+            'total reserves': r[5]
+        }
+        return mostcreditcard_dict
+
     # * ROOMTYPE
     def Get_post_RoomType(self, hid, employee_id):
         # ** Check if there is a credential
@@ -61,3 +73,49 @@ class LocalStatistics_Controller_Handler:
         for roomtype in roomtypes:
             result.append(self.RoomType_Dict(roomtype))
         return jsonify(Total_Hotel_Reserves=result), 200
+
+    # * MOSTCREDITCARD
+    def Get_post_MostCreditCard(self, hid, employee_id):
+        # ** Check if there is a credential
+        if len(employee_id) != 1:
+            return jsonify(Error="Invalid Data"), 400
+        # ** Check if the searched hotel exists
+        daoH = Hotel_Model_Dao()
+        hotel = daoH.Get_Hotel(hid)
+        if not hotel:
+            return jsonify(Error="Hotel not found"), 404
+        # ** Check if the employee exists and their information
+        eid = employee_id['eid']
+        daoE = Employee_Model_Dao()
+        employee = daoE.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found"), 404
+        daoLS = LocalStatistics_Model_Dao()
+        # ** Check employee position
+        if employee[5] == "Regular":
+            # ** Check if the employee works at the hotel that are looking for
+            if employee[1] != hid:
+                return jsonify(Error="Employee is not part of the hotel."), 404
+            mostcreditcards = daoLS.Get_post_MostCreditCard(hid)
+            result = []
+            for mostcreditcard in mostcreditcards:
+                result.append(self.MostCreditCard_Dict(mostcreditcard))
+            return jsonify(Most_Credit_Cards=result), 200
+        elif employee[5] == "Supervisor":
+            # ** Check the hotel information where the employee works
+            daoH1 = Hotel_Model_Dao()
+            hotel1 = daoH1.Get_Hotel(employee[1])
+            # ** Check if the employee works for the same chain as the searched hotel.
+            if hotel1[1] != hotel[1]:
+                return jsonify(Error=f"Hotel not part of a chain. {hotel1[1]} != {hotel[1]}"), 404
+            mostcreditcards = daoLS.Get_post_MostCreditCard(hid)
+            result = []
+            for mostcreditcard in mostcreditcards:
+                result.append(self.MostCreditCard_Dict(mostcreditcard))
+            return jsonify(Most_Credit_Cards=result), 200
+        # ** If the employee is an administrator
+        mostcreditcards = daoLS.Get_post_MostCreditCard(hid)
+        result = []
+        for mostcreditcard in mostcreditcards:
+            result.append(self.MostCreditCard_Dict(mostcreditcard))
+        return jsonify(Most_Credit_Cards=result), 200
