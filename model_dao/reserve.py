542bcb434faa67b90f8bb75663f9ef46828709cd
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-09 20:44:33 -0400

    Add method to fetch reserves by room unavailability
    A new method named 'Get_Reserve_ByRoomUnavailable' has been added to the reserve model in the DAO. This method uses the room unavailability ID (ruid) to fetch the corresponding reserve from the database.

--- model_dao/reserve.py
+++ model_dao/reserve.py
@@ -37,3 +37,20 @@ class Reserve_Model_Dao:
         self.db.close()
         cur.close()
         return reserve
+
+    """
+    ------------------
+    * TOOL OPERATIONS
+    ------------------
+    """
+
+    def Get_Reserve_ByRoomUnavailable(self, ruid):
+        cur = self.db.docker_connection.cursor()
+        query = ("SELECT reid"
+                 " FROM reserve "
+                 "WHERE ruid = %s")
+        cur.execute(query, (ruid,))
+        reserve = cur.fetchone()
+        self.db.close()
+        cur.close()
+        return reserve
