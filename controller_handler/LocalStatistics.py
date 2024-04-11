610d3485905e1155a447a1d439e83542eb93d8d6
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-10 22:15:52 -0400

    Add LeastGuests functionality to LocalStatistics controller
    Added a function to the LocalStatistics.py controller to handle LeastGuests requests. This function includes appropriate checks for employee credentials, hotel existence, and whether the employee works at the requested hotel. It provides different responses based on the position of the employee (Regular, Supervisor, or Administrator).

--- controller_handler/LocalStatistics.py
+++ controller_handler/LocalStatistics.py
@@ -234,3 +234,61 @@ class LocalStatistics_Controller_Handler:
         for leastreserve in leastreserves:
             result.append(self.LeastReserve_Dict(leastreserve))
         return jsonify(Least_Reserves=result), 200
+
+    # * LEASTGUESTS
+    def LeastGuests_Dict(self, r):
+        leastguests_dict = {
+            'hid': r[0],
+            'rid': r[1],
+            'rname': r[2],
+            'rtype': r[3],
+            'guests:capacity': r[4],
+            'space': r[5]
+        }
+        return leastguests_dict
+
+    # * LEASTGUESTS
+    def Get_post_LeastGuests(self, hid, employee_id):
+        # ** Check if there is a credential
+        if len(employee_id) != 1:
+            return jsonify(Error="Invalid Data"), 400
+        # ** Check if the searched hotel exists
+        daoH = Hotel_Model_Dao()
+        hotel = daoH.Get_Hotel(hid)
+        if not hotel:
+            return jsonify(Error="Hotel not found"), 404
+        # ** Check if the employee exists and their information
+        eid = employee_id['eid']
+        daoE = Employee_Model_Dao()
+        employee = daoE.Get_Employee(eid)
+        if not employee:
+            return jsonify(Error="Employee not found"), 404
+        daoLS = LocalStatistics_Model_Dao()
+        # ** Check employee position
+        if employee[5] == "Regular":
+            # ** Check if the employee works at the hotel that are looking for
+            if employee[1] != hid:
+                return jsonify(Error="Employee is not part of the hotel."), 404
+            leastguests = daoLS.Get_post_LeastGuests(hid)
+            result = []
+            for leastguest in leastguests:
+                result.append(self.LeastGuests_Dict(leastguest))
+            return jsonify(Least_Guests=result), 200
+        elif employee[5] == "Supervisor":
+            # ** Check the hotel information where the employee works
+            daoH1 = Hotel_Model_Dao()
+            hotel1 = daoH1.Get_Hotel(employee[1])
+            # ** Check if the employee works for the same chain as the searched hotel.
+            if hotel1[1] != hotel[1]:
+                return jsonify(Error=f"Hotel not part of a chain. {hotel1[1]} != {hotel[1]}"), 404
+            leastguests = daoLS.Get_post_LeastGuests(hid)
+            result = []
+            for leastguest in leastguests:
+                result.append(self.LeastGuests_Dict(leastguest))
+            return jsonify(Least_Guests=result), 200
+        # ** If the employee is an administrator
+        leastguests = daoLS.Get_post_LeastGuests(hid)
+        result = []
+        for leastguest in leastguests:
+            result.append(self.LeastGuests_Dict(leastguest))
+        return jsonify(Least_Guests=result), 200
