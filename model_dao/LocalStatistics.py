0330279ebc288a9499f2966e9ff90f8f1eaf5f7d
Axel Negron <axel.negron2@upr.edu>
2024-04-11 02:57:32 -0400

    Removing functions not used (functions replaced in merge)

--- model_dao/LocalStatistics.py
+++ model_dao/LocalStatistics.py
@@ -26,51 +26,6 @@ class LocalStatistics_Model_Dao:
         return result_list
 
     # * Hotel
-    def Get_post_top5_HandicapRoom_Reg(self, hid):
-        # #cur = self.db.docker_connection.cursor()
-        cur = self.dbh.heroku_connection.cursor()
-        query = (
-            "SELECT hid as hotel, rid as roomID ,rtype as room_type, count(reid) as total_reserve, ishandicap as "
-            "handicap_room "
-            "FROM reserve "
-            "natural inner join roomunavailable "
-            "natural inner join room "
-            "natural inner join roomdescription "
-            "where hid = %s and ishandicap = true "
-            "group by hid,rid, rtype, ishandicap "
-            "order by total_reserve desc")
-        cur.execute(query, (hid,))
-        result_list = cur.fetchall()
-        # self.db.close()
-        self.dbh.close()
-        cur.close()
-        return result_list
-
-    def Get_post_top5_HandicapRoom_Sup(self, hid):
-        #cur = self.db.docker_connection.cursor()
-        cur = self.dbh.heroku_connection.cursor()
-
-        chidquery = "SELECT chid FROM hotel WHERE hid = %s"
-        cur.execute(chidquery, (hid,))
-        chid = cur.fetchone()[0]
-        query = (
-            "SELECT hid as hotel, rid as roomID ,rtype as room_type, count(reid) as total_reserve, ishandicap as "
-            "handicap_room "
-            "FROM reserve "
-            "natural inner join roomunavailable "
-            "natural inner join room "
-            "natural inner join roomdescription "
-            "natural inner join hotel "
-            "where ishandicap = true and chid = %s "
-            "group by hid,rid, rtype, ishandicap "
-            "order by total_reserve desc")
-        cur.execute(query, (chid,))
-        result_list = cur.fetchall()
-        #self.db.close()
-        self.dbh.close()
-        cur.close()
-        return result_list
-
     def Get_handicap_rooms(self, hid):
         #cur = self.db.docker_connection.cursor()
         cur = self.dbh.heroku_connection.cursor()
