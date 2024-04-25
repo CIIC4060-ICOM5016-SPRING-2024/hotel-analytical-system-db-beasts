effea3c06a468b3e5aa107cb8f9a26331c9bcfce
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-25 00:34:10 -0400

    Update query in Get_post_MostDiscount method
    The SQL query in the Get_post_MostDiscount method of LocalStatistics.py has been updated to include additional calculations for discount percentage and total payments. It also includes sorting and group by operations to provide more detailed analysis based on various parameters, such as member year, last name, first name, client ID, and hotel ID.

--- model_dao/LocalStatistics.py
+++ model_dao/LocalStatistics.py
@@ -81,15 +81,21 @@ class LocalStatistics_Model_Dao:
     def Get_post_MostDiscount(self, hid):
         cur = self.db.docker_connection.cursor()
         # cur = self.dbh.heroku_connection.cursor()
-        query = ("SELECT hid, clid, fname, lname, memberyear, calculate_most_discount(memberyear) as discount "
+        query = ("SELECT hid, clid, fname, lname, memberyear, "
+                 "discount_percentage(memberyear) as discount_percentage, "
+                 "sum(total_cost) as all_payments, "
+                 "round(((sum(total_cost))/(1-discount_percentage(memberyear)))::numeric,2) as not_discount, "
+                 "round(((sum(total_cost))/(1-discount_percentage(memberyear)) - sum(total_cost))::numeric,2) "
+                 "as discount "
                  "FROM client "
                  "NATURAL INNER JOIN reserve "
                  "NATURAL INNER JOIN roomunavailable "
                  "NATURAL INNER JOIN room "
                  "NATURAL INNER JOIN hotel "
                  "WHERE hid = %s "
+                 "group by memberyear, lname, fname, clid, hid "
                  "ORDER BY discount DESC "
-                 "LIMIT  5; ")
+                 "LIMIT  5;")
         cur.execute(query, (hid,))
         result_list = cur.fetchall()
         self.db.close()
