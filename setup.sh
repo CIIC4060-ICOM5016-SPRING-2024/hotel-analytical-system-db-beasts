d2002a9014e574204b1f70ce2c5acd3b4a8d94bb
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-25 10:53:49 -0400

    streamlit

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
+#mkdir -p ~/.streamlit/
+#
+## shellcheck disable=SC2028
+#echo "\
+#[general]\n\
+#email = \"gabriel.archilla1@upr.edu\"\n\
+#" > ~/.streamlit/credentials.toml
+#
+## shellcheck disable=SC2028
+#echo "\
+#[server]\n\
+#headless = true\n\
+#enableCORS=false\n\
+#port = $PORT\n\
+#" > ~/.streamlit/config.toml
+
 mkdir -p ~/.streamlit/
-
-# shellcheck disable=SC2028
-echo "\
-[general]\n\
-email = \"gabriel.archilla1@upr.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
 # shellcheck disable=SC2028
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
