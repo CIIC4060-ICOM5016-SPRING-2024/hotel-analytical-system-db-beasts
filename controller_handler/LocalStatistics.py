08e416a3dd0bca45b73e859b8dbd44d6ebec1f83
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-25 00:40:33 -0400

    Remove unused functions in LocalStatistics.py
    The changes removed several unused functions in the LocalStatistics.py file, streamlining the codebase. The functions removed primarily dealt with room types and discounts, and were either comment-blocked or did not contribute to the current functionality of the application.

--- controller_handler/LocalStatistics.py
+++ controller_handler/LocalStatistics.py
@@ -72,63 +72,6 @@ class LocalStatistics_Controller_Handler:
         }
         return highestregpaid_dict
 
-    #     # * HIGHESTDISCOUNTS
-    # def Highest_Discounts_Dict(self, disc):
-    #     highestmemberdiscount_dict = {
-    #          'hid': disc[0],
-    #          'clid': disc[1],
-    #          'fname': disc[2],
-    #          'lname': disc[3],
-    #          'memberyear': disc[4],
-    #          'discount': disc[5]
-    #         }
-    #     return highestmemberdiscount_dict
-
-    # * ROOMTYPE
-    # def Get_post_RoomType(self, hid, employee_id):
-    #     # ** Check if there is a credential
-    #     if len(employee_id) != 1:
-    #         return jsonify(Error="Invalid Data"), 400
-    #     # ** Check if the searched hotel exists
-    #     daoH = Hotel_Model_Dao()
-    #     hotel = daoH.Get_Hotel(hid)
-    #     if not hotel:
-    #         return jsonify(Error="Hotel not found"), 404
-    #     # ** Check if the employee exists and their information
-    #     eid = employee_id['eid']
-    #     daoE = Employee_Model_Dao()
-    #     employee = daoE.Get_Employee(eid)
-    #     if not employee:
-    #         return jsonify(Error="Employee not found"), 404
-    #     daoLS = LocalStatistics_Model_Dao()
-    #     # ** Check employee position
-    #     if employee[5] == "Regular":
-    #         # ** Check if the employee works at the hotel that are looking for
-    #         if employee[1] != hid:
-    #             return jsonify(Error="Employee is not part of the hotel."), 404
-    #         roomtypes = daoLS.Get_post_RoomType(hid)
-    #         result = []
-    #         for roomtype in roomtypes:
-    #             result.append(self.RoomType_Dict(roomtype))
-    #         return jsonify(Total_Hotel_Reserves=result), 200
-    #     elif employee[5] == "Supervisor":
-    #         # ** Check the hotel information where the employee works
-    #         daoH1 = Hotel_Model_Dao()
-    #         hotel1 = daoH1.Get_Hotel(employee[1])
-    #         # ** Check if the employee works for the same chain as the searched hotel.
-    #         if hotel1[1] != hotel[1]:
-    #             return jsonify(Error=f"Hotel not part of a chain. {hotel1[1]} != {hotel[1]}"), 404
-    #         roomtypes = daoLS.Get_post_RoomType(hid)
-    #         result = []
-    #         for roomtype in roomtypes:
-    #             result.append(self.RoomType_Dict(roomtype))
-    #         return jsonify(Total_Hotel_Reserves=result), 200
-    #     # ** If the employee is an administrator
-    #     roomtypes = daoLS.Get_post_RoomType(hid)
-    #     result = []
-    #     for roomtype in roomtypes:
-    #         result.append(self.RoomType_Dict(roomtype))
-
     # Hotel
     # Top 5 handicap rooms that were reserve the most.
     def Get_post_top5_HandicapRoom(self, hid, employee_id):
@@ -389,52 +332,6 @@ class LocalStatistics_Controller_Handler:
         for highestpaid in highestpaids:
             result.append(self.Highest_Paid_Dict(highestpaid))
         return jsonify(Highest_Paid_Regular_Employees=result), 200
-
-    # ** THE TOP 5 CLIENTS WITH DISCOUNTS
-    # def Get_post_Highest_Discount_Client(self, hid, employee_id):
-    #     # ** Check if there is a credential
-    #     if len(employee_id) != 1:
-    #         return jsonify(Error="Invalid Data"), 400
-    #     # ** Check if the searched hotel exists
-    #     daoH = Hotel_Model_Dao()
-    #     hotel = daoH.Get_Hotel(hid)
-    #     if not hotel:
-    #         return jsonify(Error="Hotel not found"), 404
-    #     # ** Check if the employee exists and their information
-    #     eid = employee_id['eid']
-    #     daoE = Employee_Model_Dao()
-    #     employee = daoE.Get_Employee(eid)
-    #     if not employee:
-    #         return jsonify(Error="Employee not found"), 404
-    #     daoLS = LocalStatistics_Model_Dao()
-    #     # ** Check employee position
-    #     if employee[5] == "Regular":
-    #         # ** Check if the employee works at the hotel that are looking for
-    #         if employee[1] != hid:
-    #             return jsonify(Error="Employee is not part of the hotel."), 404
-    #         highestdiscounts = daoLS.Get_post_Most_Discount_Clients(hid)
-    #         result = []
-    #         for highestdiscount in highestdiscounts:
-    #             result.append(self.Highest_Paid_Dict(highestdiscount))
-    #         return jsonify(Highest_Most_Discount_Clients=result), 200
-    #     elif employee[5] == "Supervisor":
-    #         # ** Check the hotel information where the employee works
-    #         daoH1 = Hotel_Model_Dao()
-    #         hotel1 = daoH1.Get_Hotel(employee[1])
-    #         # ** Check if the employee works for the same chain as the searched hotel.
-    #         if hotel1[1] != hotel[1]:
-    #             return jsonify(Error=f"Hotel not part of a chain. {hotel1[1]} != {hotel[1]}"), 404
-    #         highestdiscounts = daoLS.Get_post_Most_Discount_Clients(hid)
-    #         result = []
-    #         for highestdiscount in highestdiscounts:
-    #             result.append(self.Highest_Paid_Dict(highestdiscount))
-    #         return jsonify(Highest_Most_Discount_Clients=result), 200
-    #     # ** If the employee is an administrator
-    #     highestdiscounts = daoLS.Get_post_Most_Discount_Clients(hid)
-    #     result = []
-    #     for highestdiscount in highestdiscounts:
-    #         result.append(self.Highest_Paid_Dict(highestdiscount))
-    #     return jsonify(Highest_Most_Discount_Clients=result), 200
 
     # * MOSTDISCOUNT
     def MostDiscount_Dict(self, r):
