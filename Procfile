ebf3369b39d27c18647392c7188b29e76b7f057b
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-25 10:40:18 -0400

    streamlit

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-voila: voila --port=$PORT --no-browser --Voila.ip=0.0.0.0 --Voila.base_url=/dashboard dashboard.ipynb
-web: gunicorn main:app
+# voila: voila --port=$PORT --no-browser --Voila.ip=0.0.0.0 --Voila.base_url=/dashboard dashboard.ipynb
+# gunicorn: gunicorn main:app
+web: streamlit run dashboard.py
--- dashboard.py
+++ dashboard.py
@@ -1,14 +1,11 @@
 import streamlit as st
 
+# Create a title for the login page
+st.title("Login Page")
 
-class loginpage:
-    def loginpage(self):
-        # Create a title for the login page
-        st.title("Login Page")
+# Create fields for user input
+username = st.text_input("Username")  # Requires a label
+password = st.text_input("Password", type="password")  # Requires a label; type="password" hides the text
 
-        # Create fields for user input
-        username = st.text_input("Username")  # Requires a label
-        password = st.text_input("Password", type="password")  # Requires a label; type="password" hides the text
-
-        # Button for login
-        login_button = st.button("Login")
+# Button for login
+login_button = st.button("Login")
