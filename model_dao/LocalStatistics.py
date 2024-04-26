98af11030254bdc939ee298873404bb0ad032feb
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-26 00:15:13 -0400

    Update SQL query in LocalStatistics model
    The SQL query of the method Get_post_MostDiscount in the LocalStatistics model has been updated. The new query now also calculates the sum of all payments, the non-discounted cost and the actual discount for each client. The calculated fields should provide a more detailed overview of each client's discount data.

--- model_dao/LocalStatistics.py
+++ model_dao/LocalStatistics.py
@@ -118,18 +118,21 @@ class LocalStatistics_Model_Dao:
     # * MOSTDISCOUNT
     def Get_post_MostDiscount(self, hid):
         cur = self.db.docker_connection.cursor()
-        query = ("select hid,"
-                 "       clid, "
-                 "       fname || ' ' || lname as full_name, "
-                 "       memberyear, calculate_most_discount(memberyear) as discount "
-                 "from client "
-                 "natural inner join reserve "
-                 "natural inner join roomunavailable "
-                 "natural inner join room "
-                 "natural inner join hotel "
-                 "where hid = %s "
-                 "order by discount desc "
-                 "limit 5;")
+        query = ("SELECT hid, clid, fname, lname, memberyear, "
+                 "discount_percentage(memberyear) as discount_percentage, "
+                 "sum(total_cost) as all_payments, "
+                 "round(((sum(total_cost))/(1-discount_percentage(memberyear)))::numeric,2) as not_discount, "
+                 "round(((sum(total_cost))/(1-discount_percentage(memberyear)) - sum(total_cost))::numeric,2) "
+                 "as discount "
+                 "FROM client "
+                 "NATURAL INNER JOIN reserve "
+                 "NATURAL INNER JOIN roomunavailable "
+                 "NATURAL INNER JOIN room "
+                 "NATURAL INNER JOIN hotel "
+                 "WHERE hid = %s "
+                 "group by memberyear, lname, fname, clid, hid "
+                 "ORDER BY discount DESC "
+                 "LIMIT  5;")
         cur.execute(query, (hid,))
         result_list = cur.fetchall()
         self.db.close()
