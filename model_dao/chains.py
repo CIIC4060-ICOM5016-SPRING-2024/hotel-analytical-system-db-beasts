cf020c9d27213cdfe3d3c52b51ed2a264e99328d
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-26 02:03:17 -0400

    Add support for Docker and Heroku databases in chains model DAO
    The DAO for the chains model has been extended to support both Docker and Heroku databases. By integrating the DatabaseOption function for conditional control flow, data fetching and manipulation methods now work for both interfaces. This adds robustness to the system by catering to both platform options for database handling.

--- model_dao/chains.py
+++ model_dao/chains.py
@@ -1,13 +1,16 @@
 # ** Importing Docker_Database from db module
 from config.db import Docker_Database, Heroku_Database
+from config.dboption import DatabaseOption
 
 
 # ** Class for handling database operations related to chains model
 class Chains_Model_Dao:
     def __init__(self):
         # ** Initializing database connection to Docker_Database
-        self.db = Docker_Database()
-        # self.dbh = Heroku_Database()
+        if DatabaseOption() == 'd':
+            self.db = Docker_Database()
+        elif DatabaseOption() == 'h':
+            self.dbh = Heroku_Database()
 
     """
     ------------------
@@ -17,80 +20,131 @@ class Chains_Model_Dao:
 
     # ** Method to fetch all chains from the database
     def Get_All_Chains(self):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
-        query = ("SELECT * "
-                 "FROM chains "
-                 "order by chid")
-        cur.execute(query)
-        chains_list = cur.fetchall()
-        self.db.close()
-        # self.dbh.close()
-        cur.close()
-        return chains_list
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+            query = ("SELECT * "
+                     "FROM chains "
+                     "order by chid")
+            cur.execute(query)
+            chains_list = cur.fetchall()
+            self.db.close()
+            cur.close()
+            return chains_list
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
+            query = ("SELECT * "
+                     "FROM chains "
+                     "order by chid")
+            cur.execute(query)
+            chains_list = cur.fetchall()
+            self.dbh.close()
+            cur.close()
+            return chains_list
 
     # ** Method to fetch a specific chain by its ID from the database
     def Get_Chain(self, chid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
-        query = ("SELECT * "
-                 "FROM chains "
-                 "WHERE chid = %s")
-        cur.execute(query, (chid,))
-        chain = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
-        cur.close()
-        return chain
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+            query = ("SELECT * "
+                     "FROM chains "
+                     "WHERE chid = %s")
+            cur.execute(query, (chid,))
+            chain = cur.fetchone()
+            self.db.close()
+            cur.close()
+            return chain
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
+            query = ("SELECT * "
+                     "FROM chains "
+                     "WHERE chid = %s")
+            cur.execute(query, (chid,))
+            chain = cur.fetchone()
+            self.dbh.close()
+            cur.close()
+            return chain
 
     # ** Method to add a new chain to the database
     def Post_Chain(self, cname, springmkup, summermkup, fallmkup, wintermkup):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
-        query = ("INSERT INTO chains (cname, springmkup, summermkup, fallmkup, wintermkup) "
-                 "VALUES (%s, %s, %s, %s, %s) "
-                 "returning chid")
-        cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup))
-        result = cur.fetchone()[0]
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
-        cur.close()
-        return result
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+            query = ("INSERT INTO chains (cname, springmkup, summermkup, fallmkup, wintermkup) "
+                     "VALUES (%s, %s, %s, %s, %s) "
+                     "returning chid")
+            cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup))
+            result = cur.fetchone()[0]
+            self.db.docker_connection.commit()
+            self.db.close()
+            cur.close()
+            return result
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
+            query = ("INSERT INTO chains (cname, springmkup, summermkup, fallmkup, wintermkup) "
+                     "VALUES (%s, %s, %s, %s, %s) "
+                     "returning chid")
+            cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup))
+            result = cur.fetchone()[0]
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
+            cur.close()
+            return result
 
     # ** Method to update an existing chain in the database
     def Put_Chain(self, chid, cname, springmkup, summermkup, fallmkup, wintermkup):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
-        query = ("UPDATE chains "
-                 "SET cname = %s, springmkup = %s, summermkup = %s, fallmkup = %s, wintermkup = %s "
-                 "WHERE chid = %s")
-        cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup, chid))
-        count = cur.rowcount
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
-        cur.close()
-        return count
-
-    def Delete_Chain(self, chid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
-        query = ("DELETE FROM chains "
-                 "WHERE chid = %s")
-        try:
-            cur.execute(query, (chid,))
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+            query = ("UPDATE chains "
+                     "SET cname = %s, springmkup = %s, summermkup = %s, fallmkup = %s, wintermkup = %s "
+                     "WHERE chid = %s")
+            cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup, chid))
             count = cur.rowcount
             self.db.docker_connection.commit()
-            # self.dbh.heroku_connection.commit()
             self.db.close()
-            # self.dbh.close()
             cur.close()
             return count
-        except:
-            self.db.close()
-            # self.dbh.close()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
+            query = ("UPDATE chains "
+                     "SET cname = %s, springmkup = %s, summermkup = %s, fallmkup = %s, wintermkup = %s "
+                     "WHERE chid = %s")
+            cur.execute(query, (cname, springmkup, summermkup, fallmkup, wintermkup, chid))
+            count = cur.rowcount
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
             cur.close()
-            return "Error deleting"
+            return count
+
+    def Delete_Chain(self, chid):
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+            query = ("DELETE FROM chains "
+                     "WHERE chid = %s")
+            try:
+                cur.execute(query, (chid,))
+                count = cur.rowcount
+                self.db.docker_connection.commit()
+                self.db.close()
+                cur.close()
+                return count
+            except:
+                self.db.close()
+                cur.close()
+                return "Error deleting"
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
+            query = ("DELETE FROM chains "
+                     "WHERE chid = %s")
+            try:
+                cur.execute(query, (chid,))
+                count = cur.rowcount
+                self.dbh.heroku_connection.commit()
+                self.dbh.close()
+                cur.close()
+                return count
+            except:
+                self.dbh.close()
+                cur.close()
+                return "Error deleting"
+
+
+
