d82488be099625afb7f99aa4f28f64a9969628a7
Gabriel Archilla <gabriel.archilla1@upr.edu>
2024-04-27 19:23:42 -0400

    Implement database selection option in client model
    The client model's database operations have been updated to support both Docker_Database and Heroku_Database based on the value returned by the DatabaseOption function. This includes modifications in methods initializing database connection, fetch, insert, update, delete and commit operations on the client model. This allows the client model to interact with the appropriate database based on the configuration.

--- model_dao/client.py
+++ model_dao/client.py
@@ -1,13 +1,15 @@
 # ** Importing Docker_Database from db module
-from config.db import Docker_Database, Heroku_Database
+from config.db import Docker_Database, Heroku_Database, DatabaseOption
 
 
 # ** Class for handling database operations related to client model
 class Client_Model_Dao:
     def __init__(self):
         # ** Initializing database connection to Docker_Database
-        self.db = Docker_Database()
-        # self.dbh = Heroku_Database()
+        if DatabaseOption() == 'd':
+            self.db = Docker_Database()
+        elif DatabaseOption() == 'h':
+            self.dbh = Heroku_Database()
 
     """
     ------------------
@@ -17,86 +19,116 @@ class Client_Model_Dao:
 
     # ** Method to fetch all clients from the database
     def Get_All_Clients(self):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM client  "
                  "ORDER BY clid")
         cur.execute(query)
         client_list = cur.fetchall()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return client_list
 
     # ** Method to fetch a specific client by its ID from the database
     def Get_Client(self, clid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM client "
                  "WHERE clid = %s")
         cur.execute(query, (clid,))
         client = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return client
 
+    # ** Method to add a new client to the database
     def Post_Client(self, fname, lname, age, memberyear):
-        # ** Method to add a new client to the database
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("INSERT INTO client ( fname, lname, age, memberyear)"
                  "VALUES (%s, %s, %s, %s)"
                  "returning clid")
         cur.execute(query, (fname, lname, age, memberyear))
         result = cur.fetchone()[0]
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return result
 
     def Put_Client(self, clid, fname, lname, age, memberyear):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("UPDATE client "
                  "SET fname = %s, lname = %s, age = %s, memberyear = %s "
                  "WHERE clid = %s")
         cur.execute(query, (fname, lname, age, memberyear, clid))
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return clid
 
     def Delete_Client(self, clid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("DELETE FROM client "
-                     "WHERE clid = %s")
+                 "WHERE clid = %s")
         cur.execute(query, (clid,))
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return clid
 
     def Get_Client_Reservations(self, clid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM reserve "
                  "WHERE clid = %s")
         cur.execute(query, (clid,))
         result = cur.fetchall()
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return result
