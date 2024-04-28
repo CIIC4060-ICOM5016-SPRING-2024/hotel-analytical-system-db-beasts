83441499e1d55d27da7f26e93a25ec7fd34f2eb2
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-27 20:49:44 -0400

    Add database option to RoomUnavailable model DAO
    Added an option to select between Docker_Database and Heroku_Database in RoomUnavailable Model DAO. This selection decides the database connection and associated operations. The chosen database is now used in various methods that perform fetch, post, put and delete operations.

--- model_dao/roomunavailable.py
+++ model_dao/roomunavailable.py
@@ -1,13 +1,15 @@
 # ** Importing Docker_Database from db module
-from config.db import Docker_Database, Heroku_Database
+from config.db import Docker_Database, Heroku_Database, DatabaseOption
 
 
 # ** Class for handling database operations related to roomunavailable model
 class RoomUnavailable_Model_Dao:
     def __init__(self):
         # ** Initializing database connection to Docker_Database
-        self.db = Docker_Database()
-        # self.dbh = Heroku_Database()
+        if DatabaseOption() == 'd':
+            self.db = Docker_Database()
+        elif DatabaseOption() == 'h':
+            self.dbh = Heroku_Database()
 
     """
     ------------------
@@ -17,79 +19,106 @@ class RoomUnavailable_Model_Dao:
 
     # ** Method to fetch all roomsunavailable from the database
     def Get_All_RoomsUnavailable(self):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM roomunavailable "
                  "ORDER BY ruid")
         cur.execute(query)
         roomsunavailable_list = cur.fetchall()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomsunavailable_list
 
     # ** Method to fetch a specific roomunavailable by its ID from the database
     def Get_RoomUnavailable(self, ruid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM roomunavailable "
                  "WHERE ruid = %s")
         cur.execute(query, (ruid,))
         roomunavailable = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomunavailable
 
     def Post_RoomUnavailable(self, rid, startdate, enddate):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("INSERT INTO roomunavailable (rid, startdate, enddate) "
                  "VALUES (%s, %s, %s)"
                  "returning ruid")
         cur.execute(query, (rid, startdate, enddate))
         result = cur.fetchone()[0]
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return result
 
     def Put_RoomUnavailable(self, ruid, rid, startdate, enddate):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("UPDATE roomunavailable "
                  "SET rid = %s, startdate = %s, enddate = %s "
                  "WHERE ruid = %s")
         cur.execute(query, (rid, startdate, enddate, ruid))
         count = cur.rowcount
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return count
 
     def Delete_RoomUnavailable(self, ruid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("DELETE FROM roomunavailable "
                  "WHERE ruid = %s")
         try:
             cur.execute(query, (ruid,))
             count = cur.rowcount
-            self.db.docker_connection.commit()
-            # self.dbh.heroku_connection.commit()
-            self.db.close()
-            # self.dbh.close()
+            if DatabaseOption() == 'd':
+                self.db.docker_connection.commit()
+                self.db.close()
+            elif DatabaseOption() == 'h':
+                self.dbh.heroku_connection.commit()
+                self.dbh.close()
             cur.close()
             return count
         except:
-            self.db.close()
-            # self.dbh.close()
+            if DatabaseOption() == 'd':
+                self.db.close()
+            elif DatabaseOption() == 'h':
+                self.dbh.close()
             cur.close()
             return "Error deleting"
 
@@ -101,40 +130,55 @@ class RoomUnavailable_Model_Dao:
 
     # ** Searching for the last date where the room is not available
     def RoomUnavailable_Time(self, rid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT max(enddate) as enddate "
                  "FROM roomunavailable "
                  "where rid = %s")
         cur.execute(query, (rid,))
         roomunavailable_time = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomunavailable_time
 
     def RoomUnavailable_Time_put(self, rid, ruid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT max(enddate) as enddate "
                  "FROM roomunavailable "
                  "WHERE rid = %s and ruid != %s")
         cur.execute(query, (rid, ruid))
         roomunavailable_time = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomunavailable_time
 
     def Get_Roomdateavailable(self, rid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT MAX(enddate) AS max_enddate "
                  "FROM roomunavailable "
                  "WHERE rid = %s")
         cur.execute(query, (rid,))
         room_info = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
-        cur.close()  # Close the cursor after fetching the result
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
+        cur.close()
         return room_info
