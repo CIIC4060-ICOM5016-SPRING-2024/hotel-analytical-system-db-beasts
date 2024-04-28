9aa6904687a7ced162a21613960f77c690e8888e
Gabriel Archilla <70216110+gaac-rum@users.noreply.github.com>
2024-04-27 20:46:23 -0400

    Implement dynamic database selection
    The code was updated to allow dynamic selection between Docker and Heroku databases. Depending on the provided DatabaseOption, the corresponding database gets selected, enhancing the flexibility and scalability of the application. All the methods within the RoomDescription_Model_Dao class have been updated to support this new feature.

--- model_dao/roomdescription.py
+++ model_dao/roomdescription.py
@@ -1,13 +1,15 @@
 # ** Importing Docker_Database from db module
-from config.db import Docker_Database, Heroku_Database
+from config.db import Docker_Database, Heroku_Database, DatabaseOption
 
 
 # ** Class for handling database operations related to RoomDescription model
 class RoomDescription_Model_Dao:
     def __init__(self):
         # ** Initializing database connection to Docker_Database
-        self.db = Docker_Database()
-        # self.dbh = Heroku_Database()
+        if DatabaseOption() == 'd':
+            self.db = Docker_Database()
+        elif DatabaseOption() == 'h':
+            self.dbh = Heroku_Database()
 
     """
     ------------------
@@ -17,80 +19,107 @@ class RoomDescription_Model_Dao:
 
     # ** Method to fetch all RoomDescriptions from the database
     def Get_All_RoomDescriptions(self):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT * "
                  "FROM roomdescription "
                  "ORDER BY rdid")
         cur.execute(query)
         roomdescription_list = cur.fetchall()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomdescription_list
 
     # ** Method to fetch a specific RoomDescription by its ID from the database
     def Get_RoomDescription(self, rdid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("SELECT *  "
                  "FROM roomdescription  "
                  "WHERE rdid = %s")
         cur.execute(query, (rdid,))
         roomdescription = cur.fetchone()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.close()
         cur.close()
         return roomdescription
 
     # ** Method to add a new RoomDescription to the database
     def Post_RoomDescription(self, rname, rtype, capacity, ishandicap):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("INSERT INTO roomdescription (rname, rtype, capacity, ishandicap) "
                  "VALUES (%s, %s, %s, %s)"
                  "returning rdid")
         cur.execute(query, (rname, rtype, capacity, ishandicap))
         result = cur.fetchone()[0]
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return result
 
     # ** Method to update an existing RoomDescription in the database
     def Put_RoomDescription(self, rdid, rname, rtype, capacity, ishandicap):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("UPDATE roomdescription "
                  "SET rname = %s, rtype = %s, capacity = %s, ishandicap = %s "
                  "WHERE rdid = %s")
         cur.execute(query, (rname, rtype, capacity, ishandicap, rdid))
         count = cur.rowcount
-        self.db.docker_connection.commit()
-        # self.dbh.heroku_connection.commit()
-        self.db.close()
-        # self.dbh.close()
+        if DatabaseOption() == 'd':
+            self.db.docker_connection.commit()
+            self.db.close()
+        elif DatabaseOption() == 'h':
+            self.dbh.heroku_connection.commit()
+            self.dbh.close()
         cur.close()
         return count
 
     def Delete_RoomDescription(self, rdid):
-        cur = self.db.docker_connection.cursor()
-        # cur = self.dbh.heroku_connection.cursor()
+        cur = 0
+        if DatabaseOption() == 'd':
+            cur = self.db.docker_connection.cursor()
+        elif DatabaseOption() == 'h':
+            cur = self.dbh.heroku_connection.cursor()
         query = ("DELETE FROM roomdescription "
                  "WHERE rdid = %s")
         try:
             cur.execute(query, (rdid,))
             count = cur.rowcount
-            self.db.docker_connection.commit()
-            # self.dbh.heroku_connection.commit()
-            self.db.close()
-            # self.dbh.close()
+            if DatabaseOption() == 'd':
+                self.db.docker_connection.commit()
+                self.db.close()
+            elif DatabaseOption() == 'h':
+                self.dbh.heroku_connection.commit()
+                self.dbh.close()
             cur.close()
             return count
         except:
-            self.db.close()
-            # self.dbh.close()
+            if DatabaseOption() == 'd':
+                self.db.close()
+            elif DatabaseOption() == 'h':
+                self.dbh.close()
             cur.close()
             return "Error deleting"
